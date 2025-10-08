1c0adec1007e6eae0a27b39fe7f626885c2d54af
DesaSukaMaju <rastijuni8@gmail.com>
2025-10-08 16:08:07 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// highlight nav aktif
-document.addEventListener('DOMContentLoaded', () => {
-  const nav = document.querySelectorAll('.main-nav a');
-  const path = location.pathname.split('/').pop() || 'index.html';
-  nav.forEach(a => {
-    const href = a.getAttribute('href');
-    if (href === path) a.classList.add('active');
-  });
-});
